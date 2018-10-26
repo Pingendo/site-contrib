a0bb29069a572178587f5c8af7821a8f122db654
GiorgioPingendo <giorgio.seregni@pingendo.com>
2018-10-26 23:35:43 +0200

    Update contrib.js

--- contrib.js
+++ contrib.js
@@ -1,3 +1,6 @@
 
-if(window.location.hash && window.location.hash.split("=")[0] == "#invite_token")
-	location.pathname = "/pingendocontrib/complete_signup?token="+window.location.hash.split("=")[1];
+if(window.location.hash && window.location.hash.split("=")[0] == "#invite_token"){
+	var pathname = "/pingendocontrib/complete_signup?token="+window.location.hash.split("=")[1];
+
+	location = "https://vigilant-wright-b014c5.netlify.com/"+pathname;
+}
