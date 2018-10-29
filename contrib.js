0ce87c66dd0cce0ece823ab730848de6f4900dc4
GiorgioPingendo <giorgio.seregni@pingendo.com>
2018-10-29 16:37:45 +0100

    Update contrib.js

--- contrib.js
+++ contrib.js
@@ -3,4 +3,4 @@ if(window.location.hash && window.location.hash.split("=")[0] == "#invite_token"
 	location = "https://vigilant-wright-b014c5.netlify.com/pingendocontrib/complete_signup?token="+window.location.hash.split("=")[1];
 
 if(window.location.hash && window.location.hash.split("=")[0] == "#recovery_token")
-	location = "http://localhost:4200/pingendocontrib/recovery_password?token="+window.location.hash.split("=")[1];
+	location = "https://vigilant-wright-b014c5.netlify.com/pingendocontrib/recovery_password?token="+window.location.hash.split("=")[1];
